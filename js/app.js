070338a671375a9bab257f466e60f029528389dc
FahmidaTasmia <tasmia1070@gmail.com>
2022-08-19 21:44:54 +0600

    html and js re added with some edit

--- js/app.js
+++ js/app.js
@@ -28,8 +28,9 @@ var bikeobject = {
 
 function displayServices(service) {
     const mainSection = document.getElementById('main-section');
+   const stringfied = JSON.stringify(service);
     const div = document.createElement("div");
-    div.innerHTML =`
+    div.innerHTML = `
 
     <div class="card mx-auto mt-5" style="max-width: 800px;">
     <div class="row g-0">
@@ -42,19 +43,47 @@ function displayServices(service) {
           <p class="card-text">${service.description}</p>
           <p class="card-text"><small class="text-muted">Fare per Kilo: ${service.fareperkilo}</small>  <small class="text-muted">  Capacity: ${service.capasity}</small></p>
 
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Book Now
-</button>
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" onclick ='bookingHandle(${stringfied})' data-bs-target="#exampleModal">
+            Book Now
+            </button>
         </div>
       </div>
     </div>
   </div>
 
 `
-mainSection.appendChild(div);
-console.log(service);
+    mainSection.appendChild(div);
+    console.log(service);
 }
 
 displayServices(carobject);
 displayServices(busobject);
 displayServices(bikeobject);
+
+
+//handle booking info
+
+function bookingHandle(modal){
+
+const modalBody = document.getElementById('modal-body');
+const stringifieModal = JSON.stringify(modal);
+var vehichle ="car";
+var id2 = vehichle+ "fare-input";
+
+modalBody.innerHTML = `
+<div class="card mx-auto" style="width: 340px;">
+  <img src=${modal.imageUrl} class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">description ${modal.description}</p>
+    <div class="d-colum" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit"  onclick ='calculatecost(${stringifieModal})'>Submit</button>
+            </div>
+  </div>
+</div>
+`
+}
+
+function calculatecost(obj){
+    console.log(obj)
+}
